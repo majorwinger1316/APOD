4bc1e17cc047e9e88e61b8bb17f6fca0274188ba
majorwinger1316 <ak4425@srmist.edu.in>
2025-12-28 23:12:15 +0530

    Initial Commit

---
+++ APOD/APODApp.swift
@@ -0,0 +1,17 @@
+//
+//  APODApp.swift
+//  APOD
+//
+//  Created by Akshat Dutt Kaushik on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct APODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APOD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APOD
+//
+//  Created by Akshat Dutt Kaushik on 28/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
